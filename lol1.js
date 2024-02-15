a2ccdffe1f64efd358ac69df715ce791c962466c
Stenken1 <160119086+Stenken1@users.noreply.github.com>
2024-02-15 15:54:29 +0600

    Update lol1.js

--- lol1.js
+++ lol1.js
@@ -1,9 +1,9 @@
 function myFunction(){
-	if (myImage.getAttribute("src") == "images/IMG_20231108_181900.jpg") {
-		myImage.setAttribute("src", "images/IMG_20231024_201734_201.jpg");
+	if (myImage.getAttribute("src") == "IMG_20231108_181900.jpg") {
+		myImage.setAttribute("src", "IMG_20231024_201734_201.jpg");
 	}
 	else{
-		myImage.setAttribute("src", "images/IMG_20231108_181900.jpg");
+		myImage.setAttribute("src", "IMG_20231108_181900.jpg");
 	}
 }
 
